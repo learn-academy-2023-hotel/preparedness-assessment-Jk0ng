56de0866e53595b0252dfb5c6f99555e6ee5e4ba
Joseph Kong <josephk1018@gmail.com>
2023-12-04 08:46:09 -0800

    first-edit

--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,31 @@
-import React from "react"
-
-const ModalComponent = () => {
-  return <></>
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, } from 'reactstrap';
+const ModalComponent = (props) => {
+  const [modal, setModal] = useState(false)
+  const toggle = () => {
+    setModal(!modal)
+  }
+  return (
+    <div>
+      <Button onClick={toggle} color="primary">Click Me</Button>
+      <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle}>
+          Hi {props.inputName}
+        </ModalHeader>
+        <ModalBody>
+          Lorem Ipsum
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={toggle}>
+            Submit
+          </Button>
+          <Button onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
 }
-
 export default ModalComponent
+
